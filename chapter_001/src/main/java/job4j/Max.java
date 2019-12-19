f891c55dfa7bcba1a73fc2396a3abba540259ddc
EkateronaEA <krasnovaea@gmail.com>
2019-12-18 23:48:15 -0500

    add Max and MaxTest

--- chapter_001/src/main/java/job4j/Max.java
+++ chapter_001/src/main/java/job4j/Max.java
@@ -1,4 +1,10 @@
 package job4j;
 
 public class Max {
+
+    public static int max(int left, int right) {
+        int max = left > right || left == right ? left : right;
+        return max;
+    }
+
 }
--- chapter_001/src/test/java/job4j/MaxTest.java
+++ chapter_001/src/test/java/job4j/MaxTest.java
@@ -1,5 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package job4j;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 class MaxTest {
 
+    @Test
+    public void whenMax1To2Then2() {
+        int result = Max.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax3To2Then3() {
+        int result = Max.max(2, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenElemEquals() {
+        int result = Max.max(2, 2);
+        assertThat(result, is(2));
+    }
 }

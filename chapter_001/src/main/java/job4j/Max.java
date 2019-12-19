26561a795dfb92c0850378e1c15499b94ea4f3ec
EkateronaEA <krasnovaea@gmail.com>
2019-12-19 15:35:12 -0500

    change Max

--- chapter_001/src/main/java/job4j/Max.java
+++ chapter_001/src/main/java/job4j/Max.java
@@ -3,8 +3,7 @@ package job4j;
 public class Max {
 
     public static int max(int left, int right) {
-        int max = left >= right ? left : right;
-        return max;
+        return left > right ? left : right;
     }
 
 }

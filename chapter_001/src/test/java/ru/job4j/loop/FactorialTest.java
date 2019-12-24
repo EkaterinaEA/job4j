908a135b668bb9185109012c401f92aeacbd6583
EkaterinaEA <krasnovaea95@gmail.com>
2019-12-24 00:19:45 -0500

    Add Factorial

---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactorialTest {
+
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+
+        int n = 5;
+        int expected = 120;
+        int out = Factorial.calc(n);
+        assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        //напишите здесь тест, проверяющий, что факториал для числа 0 равен 1.
+        int n = 0;
+        int expected = 1;
+        int out = Factorial.calc(0);
+        assertEquals(expected, out);
+    }
+}

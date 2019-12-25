df0ff3e445d3767e888c04049106967310d75880
EkaterinaEA <krasnovaea95@gmail.com>
2019-12-24 21:48:26 -0500

    change CheckPrimeNumber [#159467]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,10 +3,10 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
 
     public static boolean check(int finish) {
-        boolean prime = false;
-        for(int i = 2; i < Math.sqrt(finish); i++){
-            if (finish % i != 0) {
-                prime = true;
+        boolean prime = true;
+        for(int i = 2; i < finish; i++){
+            if (finish % i == 0 && finish != i) {
+                prime = false;
                 break;
             }
         }

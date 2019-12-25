aec261ae7e30713b5eaa1922227ee74be3067336
EkaterinaEA <krasnovaea95@gmail.com>
2019-12-25 14:31:18 -0500

    add Calculate and package-info.java [#159470]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculate;
+
+/**
+ * Calculate
+ *
+ * @author Ekaterina Krasnova (krasnovaea95@gmail.com)
+ */
+
+public class Calculate {
+
+    /**
+     * Main.
+     * @param args - args
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Ekaterina Krasnova (krasnovaea95@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;

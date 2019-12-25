50834cd6b5409c2ca71a0c8597438e07c5c4efeb
EkaterinaEA <krasnovaea95@gmail.com>
2019-12-24 23:18:07 -0500

    add Mortgage and MortgageTest [#159469]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public int year(double amount, int salary, double percent) {
+        int year = 1;
+        while (salary < (amount + amount*percent/100)){
+            amount = amount - (amount * percent/100);
+            year++;
+        }
+        return year;
+    }
+
+}

420109ed93cc2db548d58c7c654bfc02644f1ef8
EkaterinaEA <krasnovaea95@gmail.com>
2019-12-25 19:44:32 -0500

    change Defragment and DefragmentTest[#159486]

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+public class Defragment {
+
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            String cell = array[index];
+            if (cell == null) {
+                int lastIndex = index;
+                while (lastIndex < array.length - 1 && array[lastIndex] == null){
+                    lastIndex ++;
+                }
+                array[index] = array[lastIndex];
+                array[lastIndex] = null;
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/DefragmentTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+class DefragmentTest {
+
+    @Test
+    public void notFirstNull() {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        assertThat(compressed, is(expected));
+    }
+
+    @Test
+    public void firstNull() {
+        String[] input = {null, "I", "wanna", null, "be", null, "compressed"};
+        String[] compressed = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        assertThat(compressed, is(expected));
+    }
+
+}

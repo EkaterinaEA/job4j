ec7d4527c831b8530621c9eaee801ac768e66bdf
EkaterinaEA <krasnovaea95@gmail.com>
2020-01-03 10:12:43 -0500

    change Cat [#159507]

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -2,9 +2,32 @@ package ru.job4j.oop;
 
 public class Cat {
 
-    public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println("Cat name: " + this.name);
+        System.out.println("Cat food: " + this.food);
     }
 
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Murka");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Musya");
+        black.show();
+    }
 }

72917e662a612fc04a5b043bb78906b23661dd5b
EkaterinaEA <krasnovaea95@gmail.com>
2020-01-03 10:45:42 -0500

    Refactoring Point [#159510]

--- chapter_001/src/test/java/ru/job4j/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/PointTest.java
@@ -8,7 +8,9 @@ public class PointTest {
     @Test
     public void distance() {
         double expected = 2.82;
-        double out = Point.distance(1, 2, 3, 4);
+        Point a = new Point(1, 2);
+        Point b = new Point(3, 4);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }

65799d4b968640e7b37e7e80ae31f7b6173517ea
EkaterinaEA <krasnovaea95@gmail.com>
2020-01-03 12:37:41 -0500

    Refactoring Triangle and TriangleTest [#159511]

--- chapter_001/src/test/java/ru/job4j/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/TriangleTest.java
@@ -2,21 +2,36 @@ package ru.job4j;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.describedAs;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 class TriangleTest {
 
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+
+        Point point1 = new Point(0, 2);
+        Point point2 = new Point(2, 2);
+        Point point3 = new Point(0, 0);
+
+        Triangle triangle = new Triangle(point1, point2, point3);
+
+        double result = triangle.area();
+        assertEquals(result, 2, 0.1);
     }
 
     @Test
     public void whenDoesNotExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 10.0);
-        assertThat(result, is(false));
+        Point point1 = new Point(0, 2);
+        Point point2 = new Point(0, 3);
+        Point point3 = new Point(0, 0);
+
+        Triangle triangle = new Triangle(point1, point2, point3);
+
+        double result = triangle.area();
+        assertEquals(result, -1, 0.1);
     }
 
 }

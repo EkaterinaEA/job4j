2f4d0e1f035fe1b7b7b597db692a05a6e6487ec3
EkaterinaEA <krasnovaea95@gmail.com>
2020-01-05 19:34:15 -0500

    change Matches [#159534]

--- chapter_002/src/main/java/ru/job4j/io/Matches.java
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -8,15 +8,21 @@ public class Matches {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean user = true;
         while (matches > 0) {
             System.out.println("Количество спичек на столе: " + matches);
             System.out.print("Выберите количество спичек от 1 до 3: \n");
             int select = Integer.valueOf(input.nextLine());
+            user = !user;
             if (select < 1 || select > 3) {
                 System.out.println("Введённое число не соответствует диапазону от 1 до 3\n");
                 continue;
             } else if (matches - select <= 0) {
-                System.out.println("Вы победили!");
+                if (user == true) {
+                    System.out.println("Победил первый игрок!");
+                } else {
+                    System.out.println("Победил второй игрок!");
+                }
             }
             matches -= select;
             System.out.println();

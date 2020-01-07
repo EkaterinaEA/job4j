fbb9a562aeeefade5d2c57765ee267e3d2ffe131
EkaterinaEA <krasnovaea95@gmail.com>
2020-01-06 20:20:15 -0500

    Написать тесты на StartUI.[#212356]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -9,44 +9,13 @@ import static org.junit.Assert.assertThat;
 class StartUITest {
 
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-     //   StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
     }
 
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                item.getId(),
-                "replaced item"
-        };
-    //    StartUI.replaceItem(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item1 = new Item("item1");
-        Item item2 = new Item("item2");
-        tracker.add(item1);
-        tracker.add(item2);
-        String deleteId = item1.getId();
-        String[] answers = {deleteId};
-  //      StartUI.deleteItem(new StubInput(answers), tracker);
-        Item[] found = tracker.findAll();
-        assertNull(tracker.findById(deleteId));
-        assertNull(tracker.findByName("item1"));
-        assertThat(found.length, is(1));
-        assertThat(found[0].getName(), is("item2"));
-    }
 }

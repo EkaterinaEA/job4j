eba6b4892cd363dfe3ff75f05b4592b4fccc113d
EkaterinaEA <krasnovaea95@gmail.com>
2020-01-24 13:32:28 -0500

    Очередь с приоритетом на LinkedList[#212318]

--- chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -9,8 +9,8 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() < task.getPriority()) {
-                index++;
+            if (element.getPriority() >= task.getPriority()) {
+                index = tasks.indexOf(element);
                 break;
             }
         }

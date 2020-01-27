d77c00bce6e9a94e5e1b1601697e09be67c514ff
EkaterinaEA <krasnovaea95@gmail.com>
2020-01-27 12:46:43 -0500

    Компаратор для строк.[#212308]

--- chapter_002/src/main/java/ru/job4j/collection/StringCompare.java
+++ chapter_002/src/main/java/ru/job4j/collection/StringCompare.java
@@ -7,8 +7,6 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int result = 0;
-      //  char[] leftChar = left.toCharArray();
-      //  char[] rightChar = right.toCharArray();
         int lenth = Math.min(left.length(), right.length());
         for (int i = 0; i < lenth; i++) {
             result = Character.compare(left.charAt(i), right.charAt(i));
@@ -17,20 +15,8 @@ public class StringCompare implements Comparator<String> {
             }
         }
         if (left.length() != right.length() && result == 0) {
-            if (left.length() < right.length()) {
-                result = -1;
-            } else {
-                result = 1;
-            }
+                result = Integer.compare(left.length(), right.length());
         }
         return result;
     }
-
-
-// иван < иванов т.к. символы и, в , а ,н
-// равны,а его длина меньше,  но иван > андрей, потому что а<и
-//т.е. в первую очередь мы сравниваем символы. если у меньшего слова
-// символы совпадают с большим, то решает длина
-
-
 }

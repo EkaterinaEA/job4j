efce6478d56f960d6890ccc2baa3525ae5568fae
EkaterinaEA <krasnovaea95@gmail.com>
2020-01-31 14:20:48 -0500

    Банковские переводы.[#212314]

--- chapter_002/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_002/src/main/java/ru/job4j/bank/BankService.java
@@ -10,8 +10,8 @@ public class BankService {
     }
 
     public void addAccount(String passport, Account account) {
-        if (findByPassport(passport) != null) {
-            User user = findByPassport(passport);
+        User user = findByPassport(passport);
+        if (user != null) {
             List<Account> accounts = users.get(user);
             if (!accounts.contains(account)) {
                 users.get(user).add(account);
@@ -29,8 +29,8 @@ public class BankService {
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        if (findByPassport(passport) != null) {
-            User user = findByPassport(passport);
+        User user = findByPassport(passport);
+        if (user != null) {
             List<Account> accounts = users.get(user);
             for (Account account : accounts) {
                 if (account.getRequisite().equals(requisite)) {

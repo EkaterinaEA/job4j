8242cc5c679dc6ed90c3364eab5b99bd517a8056
EkaterinaEA <krasnovaea95@gmail.com>
2020-02-11 21:41:51 -0500

    Функциональные итерфейсы. [#212191]

---
+++ chapter_002/src/test/java/ru/job4j/lambda/CalculatorTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void whenAddUntil3() {
+        Calculator calculator = new Calculator();
+        List<Double> buffer = new ArrayList<>();
+        calculator.multiple(
+                0, 3, 1,
+                (value, index) -> (double) value + index,
+                result -> buffer.add(result)
+        );
+        assertThat(buffer, is(Arrays.asList(1D, 2D, 3D)));
+    }
+}

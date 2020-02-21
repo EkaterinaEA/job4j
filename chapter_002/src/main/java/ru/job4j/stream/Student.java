fd1f7a364ec07c0f942cda17e39350f692d55a62
EkaterinaEA <krasnovaea95@gmail.com>
2020-02-20 19:41:59 -0500

    4. Преобразование List в Map.[#212200]

--- chapter_002/src/main/java/ru/job4j/stream/Student.java
+++ chapter_002/src/main/java/ru/job4j/stream/Student.java
@@ -1,26 +1,32 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import static ru.job4j.stream.Student.ConvertCollections.convertListToMap;
 import static ru.job4j.stream.Student.School.*;
 import static ru.job4j.stream.Student.SchoolTest.*;
 
 public class Student {
 
     private final int score;
+    private final String lastName;
 
-    public Student(int score) {
+    public Student(int score, String lastName) {
         this.score = score;
+        this.lastName = lastName;
     }
 
     @Override
     public String toString() {
         return "Student{" +
                 "score=" + score +
+                ", lastName='" + lastName + '\'' +
                 '}';
     }
+
 
     public static class School {
 
@@ -37,8 +43,7 @@ public class Student {
         }
 
         public static List<Student> collect(List<Student> students,
-                                                      Predicate<Student> predicate)
-        {
+                                            Predicate<Student> predicate) {
             return students.stream()
                     .filter(predicate)
                     .collect(Collectors.<Student>toList());
@@ -61,20 +66,33 @@ public class Student {
 
     }
 
+    public static class ConvertCollections {
+
+        public static Map<String, Student> convertListToMap(List<Student> students) {
+            return students.stream().distinct().collect(
+                    Collectors.toMap(
+                            e -> e.lastName,
+                            e -> e
+                    ));
+        }
+    }
+
     public static void main(String[] args) {
 
         List<Student> students = List.of(
-                new Student(45),
-                new Student(55),
-                new Student(90),
-                new Student(75),
-                new Student(30),
-                new Student(100)
+                new Student(45, "Ivanov"),
+                new Student(55, "Petrov"),
+                new Student(90, "Nikolaev"),
+                new Student(75, "Orlova"),
+                new Student(30, "Titova"),
+                new Student(100, "Ko")
         );
 
         List<Student> classA = getClass10A(students);
         List<Student> classB = getClass10B(students);
         List<Student> classC = getClass10C(students);
+
+        Map<String, Student> studentsMap = convertListToMap(students);
 
     }
 }

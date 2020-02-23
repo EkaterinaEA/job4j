7353de743b53d84fa8e5c5194a3f82757092b866
EkaterinaEA <krasnovaea95@gmail.com>
2020-02-22 19:21:05 -0500

    2. Список адресов[#212198]

---
+++ chapter_002/src/main/java/ru/job4j/stream/Profiles.java
@@ -0,0 +1,35 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profiles {
+
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream().map(u -> u.getAddress()).collect(Collectors.toList());
+    }
+
+    public static List<Address> collectDistinctSortedByCity(List<Profile> profiles) {
+        return collect(profiles).stream().sorted(new AddressAscendingByCity()).distinct().collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+
+        Address address1 = new Address("New-York", "First Street", 10, 48);
+        Address address2 = new Address("Boston", "Second Street", 106, 48);
+
+
+        List<Profile> profiles = List.of(
+                new Profile(address2),
+                new Profile(address1),
+                new Profile(address2),
+                new Profile(address1)
+        );
+
+        List<Address> addresses = collect(profiles);
+        addresses.stream().forEach(System.out::println);
+
+        List<Address> addressesDistinctSortedByCity = collectDistinctSortedByCity(profiles);
+        addressesDistinctSortedByCity.stream().forEach(System.out::println);
+    }
+}

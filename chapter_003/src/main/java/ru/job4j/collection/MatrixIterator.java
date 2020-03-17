d23837dc66ff4a38932eeee8dbc21349f3867e34
EkaterinaEA <krasnovaea95@gmail.com>
2020-03-16 21:24:03 -0400

    Итератор для двухмерного массива int[][][#215674]

---
+++ chapter_003/src/main/java/ru/job4j/collection/MatrixIterator.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIterator implements Iterator<Integer> {
+
+    private final int[][] array;
+    private final long size;
+    private int x = 0;
+    private int y = 0;
+    private long count = 0;
+
+    public MatrixIterator(int[][] array) {
+        this.array = array;
+        this.size = Arrays.stream(array).flatMapToInt(Arrays::stream).count();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return count < this.size;
+    }
+
+    @Override
+    public Integer next() {
+        int next;
+        try {
+            next = this.array[this.x][this.y];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new NoSuchElementException("no next element");
+        }
+        if (this.array[this.x].length - 1 > this.y) {
+            this.y++;
+        } else {
+            if (this.array.length - 1 > this.x) {
+                this.x++;
+                this.y = 0;
+            }
+        }
+        count++;
+        return next;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/collection/MatrixIteratorTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+class MatrixIteratorTest {
+
+    @Test
+    void whenJaggedArray() {
+
+        MatrixIterator matrixIterator = new MatrixIterator(new int[][]{{1, 2, 3, 4}, {5, 6}});
+        Integer[] expected = new Integer[] {1, 2, 3, 4, 5, 6};
+        List<Integer> list = new ArrayList<>();
+
+        list.add(matrixIterator.next());
+        list.add(matrixIterator.next());
+        list.add(matrixIterator.next());
+        list.add(matrixIterator.next());
+        list.add(matrixIterator.next());
+        list.add(matrixIterator.next());
+
+        Integer[] actual = list.toArray(new Integer[0]);
+
+        assertThat(actual, is(expected));
+
+    }
+}

a17f6ef8c8bbfe97020b30d32635c74fba5d402e
EkaterinaEA <krasnovaea95@gmail.com>
2020-03-21 14:35:42 -0400

    Создать метод delete для односвязного списка[#215663]

---
+++ chapter_003/src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -0,0 +1,62 @@
+package ru.job4j.collection.list;
+
+public class SimpleLinkedList<E> {
+
+    private int size;
+    private Node<E> first;
+
+    /**
+     * Метод вставляет в начало списка данные.
+     */
+    public void add(E data) {
+        Node<E> newLink = new Node<>(data);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    /**
+     * Метод удаления первого элемент в списке.
+     */
+    public E delete() {
+        E deleted = null;
+        if (size > 0) {
+            deleted = this.first.data;
+            this.first.data = null;
+            this.first = this.first.next;
+            size--;
+        }
+        return deleted;
+    }
+
+    /**
+     * Метод получения элемента по индексу.
+     */
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+
+    /**
+     * Метод получения размера коллекции.
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Класс предназначен для хранения данных.
+     */
+    private static class Node<E> {
+
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/collection/list/SimpleLinkedListTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.collection.list;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SimpleLinkedListTest {
+
+    private SimpleLinkedList<Integer> list;
+
+    @Before
+    public void beforeTest() {
+        list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+        assertThat(list.get(1), is(2));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetSizeResultThree() {
+        assertThat(list.getSize(), is(3));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenDelete() {
+        assertThat(list.delete(), is(3));
+        assertThat(list.get(0), is(2));
+        assertThat(list.get(1), is(1));
+        assertThat(list.getSize(), is(2));
+        list.delete();
+        list.delete();
+        Assert.assertNull(list.delete());
+    }
+}
+

f464992036405a81526436dd74c62db7b448c052
EkaterinaEA <krasnovaea95@gmail.com>
2020-03-26 19:38:53 -0400

    Не перекрывать equals hashCode[#215687]

--- chapter_003/src/main/java/ru/job4j/collection/map/User.java
+++ chapter_003/src/main/java/ru/job4j/collection/map/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.collection.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Map;
 
 public class User {
 
@@ -8,4 +10,25 @@ public class User {
     private int children;
     private Calendar birthday;
 
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", children=" + children +
+                '}';
+    }
 }
+
+
+
+//Создать два объекта User, которые имеют одинаковые поля.
+//
+//Создать карту Map<User, Object>
+//
+//Добавить два объекта. Вывести карту на печать. Описать полученный результат словами.
---
+++ chapter_003/src/test/java/ru/job4j/collection/map/UserTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection.map;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void whenEqualsAnsHashCodeNotRedefined() {
+        User user1 = new User("user", 0, new GregorianCalendar(2017, 0, 25));
+        User user2 = new User("user", 0, new GregorianCalendar(2017, 0, 25));
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+
+        assertThat(map.size(), is(2));
+        System.out.println(map);
+        System.out.println(map.get(user1));
+        System.out.println(map.get(user2));
+    }
+
+}

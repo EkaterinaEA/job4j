5ced3ff79adea03b135c607150e320af05d0969b
EkaterinaEA <krasnovaea95@gmail.com>
2020-04-22 11:43:11 -0400

    Тестирование IO [#215700]

--- chapter_004/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_004/src/main/java/ru/job4j/io/Analizy.java
@@ -10,13 +10,13 @@ public class Analizy {
         BufferedReader read = new BufferedReader(new FileReader(source));
         read.lines().filter(line -> !line.isEmpty()).forEach(line -> {
             try (FileWriter out = new FileWriter(target, true)){
-            if ((line.startsWith("500") || line.startsWith("400")) && !check) {
-                out.write(line.substring(4) + ";");
-                check = true;
-            } else if (check && (line.startsWith("200") || line.startsWith("300"))) {
-                out.write(line.substring(4) + System.getProperty("line.separator"));
-                check = false;
-            }
+                if ((line.startsWith("500") || line.startsWith("400")) && !check) {
+                    out.write(line.substring(4) + ";");
+                    check = true;
+                } else if (check && (line.startsWith("200") || line.startsWith("300"))) {
+                    out.write(line.substring(4) + System.getProperty("line.separator"));
+                    check = false;
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- chapter_004/src/test/java/ru/job4j/AnalizyTest.java
+++ chapter_004/src/test/java/ru/job4j/AnalizyTest.java
@@ -1,29 +1,47 @@
 package ru.job4j;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import ru.job4j.io.Analizy;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
 
-    private static final String MAIN_DIR = System.getProperty("user.dir");
-    private static final String SEPARATOR = System.getProperty("file.separator");
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
-    private static final String target = MAIN_DIR + SEPARATOR + "data" + SEPARATOR + "unavailable.csv";
-    private static final String source = MAIN_DIR + SEPARATOR + "data" + SEPARATOR + "server.log";
+    @Test
+    public void checkWithTemporaryFolder() throws IOException {
+        File source = folder.newFile("unavailable.csv");
+        File target = folder.newFile("server.log");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
+        new Analizy().unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        try (BufferedReader read = new BufferedReader(new FileReader(target))) {
+            assertThat(read.readLine(), is("10:57:01;10:59:01"));
+            assertThat(read.readLine(), is("11:01:02;11:02:02"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void check() throws IOException {
-        new Analizy().unavailable(source, target);
-        try (BufferedReader read = new BufferedReader(new FileReader(target))
-        ) {
-            assertThat(read.readLine(), is("10:58:01;10:59:01"));
+        String target = "./data/unavailable.csv";
+        new Analizy().unavailable("./data/server.log", target);
+        try (BufferedReader read = new BufferedReader(new FileReader(target))) {
+            assertThat(read.readLine(), is("10:57:01;10:59:01"));
             assertThat(read.readLine(), is("11:01:02;11:02:02"));
         } catch (Exception e) {
             e.printStackTrace();

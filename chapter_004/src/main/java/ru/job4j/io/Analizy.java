ffe53309aa01c8904b9221ccb084d57d10a24c6f
EkaterinaEA <krasnovaea95@gmail.com>
2020-04-27 16:11:47 -0400

    Анализ доступности сервера.[#215702]

--- chapter_004/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_004/src/main/java/ru/job4j/io/Analizy.java
@@ -8,8 +8,9 @@ public class Analizy {
 
     public void unavailable(String source, String target) throws IOException {
         BufferedReader read = new BufferedReader(new FileReader(source));
-        read.lines().filter(line -> !line.isEmpty()).forEach(line -> {
-            try (FileWriter out = new FileWriter(target, true)){
+        String line = read.readLine();
+        try (FileWriter out = new FileWriter(target, true)) {
+            while (line != null) {
                 if ((line.startsWith("500") || line.startsWith("400")) && !check) {
                     out.write(line.substring(4) + ";");
                     check = true;
@@ -17,9 +18,8 @@ public class Analizy {
                     out.write(line.substring(4) + System.getProperty("line.separator"));
                     check = false;
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+                line = read.readLine();
             }
-        });
+        }
     }
 }

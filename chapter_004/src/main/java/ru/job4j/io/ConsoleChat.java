45aef3f001541e2ce97f8ccc1d22b7e520d47f70
EkaterinaEA <krasnovaea95@gmail.com>
2020-05-21 21:15:07 -0400

    Создать программу 'Консольный чат'. [862#215704]

--- chapter_004/src/main/java/ru/job4j/io/ConsoleChat.java
+++ chapter_004/src/main/java/ru/job4j/io/ConsoleChat.java
@@ -1,77 +1,54 @@
 package ru.job4j.io;
 
-import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
-
-import static ru.job4j.io.ReadFile.readFile;
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class ConsoleChat {
 
-    private static String logFile = "data\\ChatLog.txt";
-    private static boolean check = true;
-    private String[] inputTestData;
-    private String[] answers = readFile("data\\Sonnet2");
+    private static final String FINISH = "finish";
+    private static final String CONTINUE = "continue";
+    private static final String STOP = "stop";
 
-    public ConsoleChat() {
+    private final String answerFile;
+    private final String logFile;
+    private Input source;
 
+    public ConsoleChat(Input input, String answerFile, String logFile) {
+        this.source = input;
+        this.answerFile = answerFile;
+        this.logFile = logFile;
     }
 
-    public ConsoleChat(String[] inputTestData) {
-        this.inputTestData = inputTestData;
-    }
+    public void init() {
+        try (PrintWriter out = new PrintWriter(new File(logFile));
+             BufferedReader in = new BufferedReader(new FileReader(answerFile))) {
+            String input;
+            boolean check = true;
+            Random random = new Random();
+            List<String> answers = in.lines().collect(Collectors.toList());
+            int size = answers.size();
+            while (!FINISH.equals((input = source.getQuestion()))) {
+                out.println(input);
 
-    public void consoleChat() throws IOException {
-        System.out.println("Добро пожаловать в консольный чат!");
-        if (this.inputTestData != null) {
-            testMethod();
-        } else workMethod();
-    }
-
-    private void workMethod() {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        while (!input.equals("закончить")) {
-            input = sc.nextLine();
-            writeLog(input);
-            if (input.equals("продолжить")) {
-                check = true;
+                if (STOP.equals(input)) {
+                    check = false;
+                }
+                if (CONTINUE.equals(input)) {
+                    check = true;
+                }
+                if (check) {
+                    String answer = answers.get(random.nextInt(size));
+                    System.out.println(answer);
+                    out.println(answer);
+                }
             }
-            if (input.equals("стоп") || input.equals("закончить")) {
-                check = false;
-            }
-            if (check) {
-                String answer = answers[(int) (Math.random() * answers.length)];
-                writeLog(answer);
-                System.out.println(answer);
-            }
-        }
-    }
-
-    private void testMethod() {
-        int position = 0;
-        String input = inputTestData[position++];
-        while (!input.equals("закончить")) {
-            input = inputTestData[position++];
-            writeLog(input);
-            if (input.equals("продолжить")) {
-                check = true;
-            }
-            if (input.equals("стоп") || input.equals("закончить")) {
-                check = false;
-            }
-            if (check) {
-                String answer = answers[(int) (Math.random() * answers.length)];
-                writeLog(answer);
-                System.out.println(answer);
-            }
-        }
-    }
-
-    private static void writeLog(String line) {
-        try (FileWriter writer = new FileWriter(logFile, true)) {
-            writer.write(line + System.lineSeparator());
-            writer.flush();
+            out.println(input);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
--- chapter_004/src/test/java/ru/job4j/ConsoleChatTest.java
+++ chapter_004/src/test/java/ru/job4j/ConsoleChatTest.java
@@ -4,39 +4,41 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import ru.job4j.io.ConsoleChat;
+import ru.job4j.io.StubInput;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class ConsoleChatTest {
-
-    private static String logFile = "data\\ChatLog.txt";
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void goAndGet() throws IOException {
-        String[] answersOfUser = {"", "привет", "как дела?", "стоп", "привет", "продолжить", "как дела?", "закончить"};
-        new ConsoleChat(answersOfUser).consoleChat();
-        try (BufferedReader in = new BufferedReader(new FileReader(logFile))){
+    public void trackerTest() throws IOException {
+        String[] answersOfUser = {"Hello!", "stop", "How are you?", "continue", "finish"};
+        String source = "data\\Sonnet2";
+        File destination = folder.newFile("log.txt");
+        new ConsoleChat(new StubInput(answersOfUser), source, destination.getAbsolutePath()).init();
+        try (BufferedReader in = new BufferedReader(new FileReader(destination))) {
             List<String> list = in.lines().collect(Collectors.toList());
-            assertThat(list.get(0), is("привет"));
+            System.out.println(list.toString());
+            assertThat(list.get(0), is("Hello!"));
             assertTrue(list.get(1).length() > 0);
-            assertThat(list.get(2), is("как дела?"));
-            assertTrue(list.get(3).length() > 0);
-            assertThat(list.get(4), is("стоп"));
-            assertThat(list.get(5), is("привет"));
-            assertThat(list.get(6), is("продолжить"));
-            assertTrue(list.get(7).length() > 0);
-            assertThat(list.get(8), is("как дела?"));
-            assertTrue(list.get(9).length() > 0);
-            assertThat(list.get(10), is("закончить"));
-            assertThat(list.size(), is(11));
+            assertThat(list.get(2), is("stop"));
+            assertThat(list.get(3), is("How are you?"));
+            assertThat(list.get(4), is("continue"));
+            assertTrue(list.get(5).length() > 0);
+            assertThat(list.get(6), is("finish"));
+            assertThat(list.size(), is(7));
         }
 
     }
